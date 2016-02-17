4039c2b756d52a70c2a6df52be704370d119cd57
Joey Nguyen <jnn193@gmail.com>
2016-02-16 23:49:14 -0600

    Added pressure and humidy charts for each city returned from search.

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import _ from 'lodash';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
-export default ({ data, color }) => {
+function average(data) {
+    return _.round(_.sum(data)/data.length);
+}
+export default ({ data, color, units }) => {
     return (
-        <Sparklines height={120} width={180} data={data}>
-            <SparklinesLine color={color} />
-        </Sparklines>
+        <div>
+            <Sparklines height={120} width={180} data={data}>
+                <SparklinesLine color={color} />
+                <SparklinesReferenceLine type="avg" />
+            </Sparklines>
+            <div>Average: {average(data)} {units}</div>
+        </div>
     );
 }
